0909f120ea7a8a9440dda76cbefeab9d0e57ee5d
oasaph <contato@asaph.dev>
2025-05-27 17:56:19 -0300

    🔖 Bump version to 0.2.0

--- flaggle/__init__.py
+++ flaggle/__init__.py
@@ -2,6 +2,6 @@ from flaggle.flaggle import Flaggle
 from flaggle.flag import Flag, FlagOperation, FlagType
 
 __all__ = ["FlagType", "FlagOperation", "Flag", "Flaggle"]
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Asaph Diniz"
 __email__ = "contato@asaph.dev.br"
